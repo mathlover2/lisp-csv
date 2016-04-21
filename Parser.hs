b827db69b62fd9dbd18c19b8f7bc9e3e50c4c95f
Brian Rich <mathlover2@gmail.com>
2016-04-20 23:50:23 -0400

    Completed first version of Parser.hs

--- Parser.hs
+++ Parser.hs
@@ -1,4 +1,4 @@
-module Parser (parseToGrades)
+module Parser
        where
 
 import Text.Parsec
@@ -7,15 +7,15 @@ import Text.Parsec
 -- objects.
 
 
-parseToGrades :: String -> IO [(String, String, String, [Int],Int)]
+parseToGrades :: String -> IO [(String,[Int],Int)]
 parseToGrades str
-  = do x <- printErrorOrReturn (parse allButEnd "" str)
+  = do x <- getGradePortionOfString str
        let y = makeReplacements x
        z <- breakUp y
        return $ toGrades z
 
-getGradePortionOfFile :: String -> IO String
-getGradePortionOfFile str = printErrorOrReturn (parse allButEnd "" str)
+getGradePortionOfString :: String -> IO String
+getGradePortionOfString str = printErrorOrReturn (parse allButEnd "" str)
 
 allButEnd =
   do spaces
@@ -25,11 +25,89 @@ allButEnd =
      closeParen
      return result
 
+makeReplacements :: String -> String
+makeReplacements x =
+  case findSharpIndex x
+  of   Just (n,s) -> return (replace sexpt s x)
+                     >>= replace sexp1 s
+         where sexpt = "#" ++ n ++ "#"
+               sexp1 = "#" ++ n ++ "=" ++ s
+       Nothing -> x
+
+breakUp str = printErrorOrReturn $ breakUp' str
+breakUp' = parse (do openParen
+                     l <- many $
+                          do x <- readParen'd
+                             spaces
+                             return x
+                     closeParen
+                     spaces
+                     eof
+                     return l) ""
+
+toGrades = map toGrade
+  where toGrade str = case toGrade' str
+                      of   Left a -> error "Bad parse"
+                           Right x -> x
+        toGrade'
+          = parse
+            ( do openParen
+                 string ":|NAME|"
+                 spaces
+                 name <- quotedString 
+                 spaces
+                 string ":|SCORES|"
+                 spaces
+                 openParen
+                 scores <- (sepBy1 pern spaces)
+                 closeParen
+                 spaces
+                 string ":|GRADE|"
+                 spaces
+                 grade <- number
+                 period
+                 closeParen
+                 eof
+                 return (name,map read scores,read grade)
+            ) ""
+        pern = do x <- number
+                  period
+                  return x
+                 
+findSharpIndex str = let x = dropWhile (/='#') str
+                     in  case (parse sharpExpression "" x)
+                         of Left _ -> Nothing
+                            Right a -> Just a
+
+sharpExpression = do sharp
+                     n <- number
+                     equalsSign
+                     x <- number <|> quotedString <|> readParen'd
+                     return (n, x)
+
 openParen = char '('
 sharp = char '#'
 equalsSign = char '='
-quote = char '"'
+quote = char '\"'
 closeParen = char ')'
+period = string "."
+
+number = many1 digit
+
+quotedString = do quote
+                  x <- stringContents
+                  quote
+                  return x
+
+stringContents = fmap concat $ many (many1 (satisfy (/='\"')) <|> string "\\\"")
+
+replace = replace' []
+replace' acc _ _ [] = reverse acc
+replace' acc old new target@(t0:ts) =
+  if take len target == old
+  then replace' acc old new (new++drop len target)
+  else replace' (t0:acc) old new ts
+       where len = length old
 
 readParen'd =
   do openParen
@@ -39,8 +117,9 @@ readParen'd =
 
 parenCompound = many1 (noneOf "()") <|> readParen'd
 
-printErrorOrReturn :: Either ParseError String -> IO String
+printErrorOrReturn :: Either ParseError a -> IO a
 printErrorOrReturn result =
      case result of
-       Left err -> error $ "Parse failed: Exception was ++ \"" ++ show err ++ "\""
+       Left err -> error $ "Parse failed: Exception was ++ \""
+                   ++ show err ++ "\""
        Right xs -> return xs
